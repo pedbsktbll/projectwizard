626b298fc01d5a777eb3f405f3401fe537307dd4
WantedHiker <wantedhiker@outlook.com>
2013-09-29 10:21:56 -0400

    Created a bunch of user control objects that will contain all the information we need.  They will be shown in the following order:
    1. ucType - Shown at startup.
        a. Gives users the option of project type (exe, dll, sys, lib, and special projects).
        b. Option to name the main cpp file (defaults to project name).
        c. The compiler version (2013, 2012, 2010, 2008) will hopefully top out at the currently running version of vs.
    
    2. ucSubmodules.
        a. Auto generated list of submodules from an xml file.  Populated in a multi checkbox
        b. Description of the currently selected submodule.
    
    3.  ucAuthorBlock
        a. Tool Name
        b. Requirement Number
        c.  Customer
        d.  Version start (0.1 or 1.0)
        e.  Authors
        f.  Description
        g. Status
    
    4.  ucConfirmation
        a.  Reiterates everything that was selected.
    
    5.  usProgress
        a.  Dialog box displaying everything that is happening.

--- ProjectWizard/fMain.Designer.cs
+++ ProjectWizard/fMain.Designer.cs
@@ -28,47 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pbLeftBanner = new System.Windows.Forms.PictureBox();
             this.pBottom = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbLeftBanner = new System.Windows.Forms.PictureBox();
+            this.pBottom.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbLeftBanner)).BeginInit();
             this.SuspendLayout();
             // 
+            // pBottom
+            // 
+            this.pBottom.Controls.Add(this.button3);
+            this.pBottom.Controls.Add(this.button2);
+            this.pBottom.Controls.Add(this.bCancel);
+            this.pBottom.Controls.Add(this.button1);
+            this.pBottom.Location = new System.Drawing.Point(150, 525);
+            this.pBottom.Name = "pBottom";
+            this.pBottom.Size = new System.Drawing.Size(685, 30);
+            this.pBottom.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(437, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "&Back";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(518, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "&Next";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // bCancel
+            // 
+            this.bCancel.Location = new System.Drawing.Point(599, 3);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.TabIndex = 0;
+            this.bCancel.Text = "&Cancel";
+            this.bCancel.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(518, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "C&reate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // pbLeftBanner
             // 
+            this.pbLeftBanner.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbLeftBanner.Image = global::ProjectWizard.Properties.Resources.temp;
             this.pbLeftBanner.Location = new System.Drawing.Point(0, 0);
             this.pbLeftBanner.Name = "pbLeftBanner";
             this.pbLeftBanner.Size = new System.Drawing.Size(150, 555);
+            this.pbLeftBanner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbLeftBanner.TabIndex = 0;
             this.pbLeftBanner.TabStop = false;
-            // 
-            // pBottom
-            // 
-            this.pBottom.Location = new System.Drawing.Point(150, 500);
-            this.pBottom.Name = "pBottom";
-            this.pBottom.Size = new System.Drawing.Size(685, 55);
-            this.pBottom.TabIndex = 1;
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(150, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(685, 500);
-            this.panel1.TabIndex = 2;
             // 
             // fMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(836, 555);
             this.ControlBox = false;
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pBottom);
             this.Controls.Add(this.pbLeftBanner);
             this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "fMain";
             this.Text = "Project Creation Wizard";
+            this.pBottom.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbLeftBanner)).EndInit();
             this.ResumeLayout(false);
 
@@ -78,7 +119,10 @@
 
         private System.Windows.Forms.PictureBox pbLeftBanner;
         private System.Windows.Forms.Panel pBottom;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ ProjectWizard/ucAuthorBolck.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ProjectWizard
+{
+    partial class ucAuthorBlock
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucAuthorBlock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.Name = "ucAuthorBlock";
+            this.Size = new System.Drawing.Size(685, 525);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
