acdc4f92bd453bb6ef2771079b9ab69b430dd76b
Josh <joshschulte1@gmail.com>
2013-11-10 17:47:25 -0500

    Fixed up the resource files... All projects will successfully build upon wizard creation!

--- ProjectWizard/Resources/base/DLLApp/dllmain.cpp
+++ ProjectWizard/Resources/base/DLLApp/dllmain.cpp
@@ -1,9 +1,9 @@
 /*
  * Filename:		dllmain.cpp
  *
- * Author:			$_____USER_____
- * Date Created:	$_____DATE_____
- * Version _____VERSION_____:		$_____DATE_____ ($_____USER_____)
+ * Author:			_____USER_____
+ * Date Created:	_____DATE_____
+ * Version _____VERSION_____:		_____DATE_____ (_____USER_____)
  *
  * _____DESCRIPTION_____
  */
