687c8c81a5f71603739682ce9b12b260ad344446
WantedHiker <wantedhiker@outlook.com>
2013-11-11 18:44:25 -0500

    Slammed into a brick wall with the DLL I wanted to use, we are going to have to go with relying on the git installed on the system.  Below is a quick class I put together to call the git commands we need, but I have to work on the kinks.  Going to be running around tonight, probably won't get much more of a chance to work on it until Tomorrow, but I figured I'd upload so you can see it.

---
+++ ProjectWizard/GitInterop.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace ProjectWizard
+{
+    internal class GitInterop
+    {
+        private Process Proc = new Process();
+        private ProcessStartInfo ProcInfo = new ProcessStartInfo();
+        private StreamWriter inputWriter;
+        private StreamReader errorReader;
+        private StreamReader outputReader;
+
+        public GitInterop(string workingDirectory)
+        {
+            //TODO figure out where git is installed on the system.
+            ProcInfo.FileName = @"C:\Program Files (x86)\Git\bin\sh.exe";
+            ProcInfo.Arguments = "--login -i";
+            ProcInfo.RedirectStandardInput = true;
+            ProcInfo.RedirectStandardError = true;
+            ProcInfo.RedirectStandardOutput = true;
+            ProcInfo.UseShellExecute = false;
+            ProcInfo.CreateNoWindow = true;
+            ProcInfo.WorkingDirectory = workingDirectory;
+            Proc.StartInfo = ProcInfo;
+        }
+
+        ~GitInterop()
+        {
+            Proc.Close();
+        }
+
+        public bool init()
+        {
+            Proc.Start();
+            inputWriter = Proc.StandardInput;
+            errorReader = Proc.StandardError;
+            outputReader = Proc.StandardOutput;
+            inputWriter.WriteLine("git init");
+            inputWriter.Flush();
+            inputWriter.WriteLine("exit");
+            inputWriter.Flush();
+            Proc.WaitForExit();
+            return true;
+        }
+
+        public bool Remote_Add(string remotePath, string remoteName = "origin")
+        {
+            Proc.Start();
+            inputWriter = Proc.StandardInput;
+            errorReader = Proc.StandardError;
+            outputReader = Proc.StandardOutput;
+            inputWriter.WriteLine("git remote add {0} {1}", remoteName, remotePath);
+            inputWriter.Flush();
+            inputWriter.WriteLine("exit");
+            inputWriter.Flush();
+            Proc.WaitForExit();
+            return true;
+        }
+
+        public bool Submodule_Add(string submoduleAddress, string submodulePath)
+        {
+            Proc.Start();
+            inputWriter = Proc.StandardInput;
+            errorReader = Proc.StandardError;
+            outputReader = Proc.StandardOutput;
+            inputWriter.WriteLine("git submodule add {0} {1}", submoduleAddress, submodulePath);
+            inputWriter.Flush();
+            inputWriter.WriteLine("exit");
+            inputWriter.Flush();
+            Proc.WaitForExit();
+            return true;
+        }
+    }
+}
