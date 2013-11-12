560a68b7e6aa7c39e87398d13ade0a6e392afb24
Josh <joshschulte1@gmail.com>
2013-11-11 20:40:15 -0500

    yay my ghetto ass installer works!

--- ProjectInstaller/Program.cs
+++ ProjectInstaller/Program.cs
@@ -15,7 +15,8 @@ namespace ProjectInstaller
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+//			Application.Run(new Form1());
+			new ProjInstaller();
 		}
 	}
 }
